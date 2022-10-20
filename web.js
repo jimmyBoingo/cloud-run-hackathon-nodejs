3b04e9b199c7173b2883c5761ff448284858dab3
jimmyBoingo <jtang@boingo.com>
2022-10-20 11:26:15 -0700

    Update web.js

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ app.post('/', function (req, res) {
   
   
   //res.send(moves[Math.floor(Math.random() * moves.length)]);
-  res.send('L','R');
+  res.send('L');
 });
 
 app.listen(process.env.PORT || 8080);
