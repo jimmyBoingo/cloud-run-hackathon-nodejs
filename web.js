77319e6a56f0f51c4c25c5260c8b453246f91e05
jimmyBoingo <jtang@boingo.com>
2022-10-20 11:22:50 -0700

    Update web.js

--- web.js
+++ web.js
@@ -12,9 +12,10 @@ app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
   
-  // TODO add your implementation here to replace the random response
   
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  
+  //res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send('L');
 });
 
 app.listen(process.env.PORT || 8080);
